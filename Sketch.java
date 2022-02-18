a2624fc1a06b0677b43ca2fd7ac8b1fc499cf27b
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2022-02-18 09:26:00 -0500

    First Commit

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(600, 600);
   }
 
   /** 
@@ -16,7 +16,8 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(139,231,139);
+  
   }
 
   /**
@@ -24,12 +25,43 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Set stroke weight and initial color  
+        
+    strokeWeight(4);
+    fill(0,0,0);
+    
+    // Draw panda ears
+    arc(175, 180, 200, 200, HALF_PI + QUARTER_PI, PI + HALF_PI + QUARTER_PI + radians(5), OPEN);
+    arc(425, 180, 200, 200, PI + QUARTER_PI - radians(5), PI + PI + QUARTER_PI, OPEN);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Draw panda face
+    fill(255,255,255);
+    ellipse(300, 300, 450, 375);  
+    
+    // Draw Panda Eyes
+    fill(0,0,0);
+    arc(200, 295, 150, 150, HALF_PI + QUARTER_PI + radians(14), PI + HALF_PI + QUARTER_PI-radians(14), OPEN);
+    arc(175, 270, 150, 150, radians(-32), HALF_PI + QUARTER_PI - radians(13), OPEN);
+
+    arc(400, 295, 150, 150, PI + QUARTER_PI + radians(14), PI + PI+QUARTER_PI - radians(14), OPEN);
+    arc(425, 270, 150, 150, HALF_PI + radians(-32), PI+QUARTER_PI - radians(13), OPEN);
+
+    fill(255,255,255);
+    ellipse(210, 262, 30,30);
+    ellipse(390, 262, 30,30);
+    
+
+    // Draw panda nose
+    fill(0,0,0);
+    ellipse(300, 375, 75, 40);
+    
+    // Draw "Whiskers"
+    noFill();
+    strokeWeight(8);
+    line(300, 375, 300, 405);
+    arc(275, 405, 50, 40, 0, HALF_PI + QUARTER_PI);
+    arc(325, 405, 50, 40, QUARTER_PI, PI);
+     
   }
   
   // define other methods down here.
